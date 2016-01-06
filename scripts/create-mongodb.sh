c9e50a09b42301b3f970c675a19b475fdf052d0a
niallbrown <niall.brown@lightspeedpos.com>
2016-01-06 15:33:11 -0500

    update

--- scripts/create-mongodb.sh
+++ scripts/create-mongodb.sh
@@ -2,7 +2,32 @@
 #mongo --eval 'use admin'
 #mongo --eval 'db.createUser({ user: "admin", pwd: "admin", roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]})'
 
-sudo apt-get install -y libcurl4-openssl-dev libsasl2-dev
-echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+apt-get install -y libcurl4-openssl-dev libsasl2-dev
+
+pecl install -f mongodb-3.0.8
+
+if [ "$(cat /etc/php5/cli/conf.d/20-mongo.ini | grep 'mongodb.so')" ];
+    then
+        echo "extension already loaded"
+    else
+        #echo 'extension = mongodb.so' >> ~/etc/php5/cli/conf.d/20-mongo.ini;
+        echo "need to load extension cli"
+fi
+
+if [ "$(cat /etc/php5/fpm/conf.d/20-mongo.ini | grep 'mongodb.so')" ];
+    then
+        echo "extension already loaded"
+    else
+        #echo 'extension = mongodb.so' >> ~/etc/php5/fpm/conf.d/20-mongo.ini;
+           echo "need to load extension in fpm"
+fi
+
+#echo 'db.createUser({user: "admin", pwd: "admin", roles: []);' | mongo admin
+
+#mongo --eval 'use admin'
+#mongo --eval 'db.createUser({ user: "admin", pwd: "admin", roles: [ db:  "admin"]})'
+
+#echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+
 service nginx restart
 service php5-fpm restart
